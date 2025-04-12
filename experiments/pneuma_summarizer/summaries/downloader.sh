87d7f87b0c09fc66ce5a2746043aaf7372f2b73e
luthfibalaka <luthfibalaka@gmail.com>
2025-04-12 09:04:06 -0500

    Migrate downloader script to bash

---
+++ experiments/pneuma_summarizer/summaries/downloader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+prefix="https://uchicago.box.com/shared/static/"
+full_url="${prefix}yy3y3gc3b9flmd760jbhkqar6mhfc4eq"
+wget --content-disposition -P "$script_dir" "$full_url"
+zip_file=$(find "$script_dir" -maxdepth 1 -name "*.zip" -print -quit)
+
+if [[ -f "$zip_file" ]]; then
+    unzip "$zip_file" -d "$script_dir"
+    rm "$zip_file"
+else
+    echo "No ZIP file found. Something went wrong."
+fi
